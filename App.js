eceb300aafcde4f553fb878ce252031604561b4f
Josh Eskew <jeskew82@gmail.com>
2020-11-02 19:43:20 -0500

    added app folder. Moved assets into it. Also, added the WelcomeScreen file as well

--- App.js
+++ App.js
@@ -1,27 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  console.log('app working');
-
-
-  return (
-
-
-
-    <View style={styles.container}>
-      <Text>Hey Ted</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
